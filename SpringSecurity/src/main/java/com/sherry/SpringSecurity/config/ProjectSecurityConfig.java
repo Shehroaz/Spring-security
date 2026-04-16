65fa422f0917bd60b410c638254861d847e6219c
Shehroaz Aslam <muhammadshehroaz@gmail.com>
2026-04-16 12:18:04 +0500

    Spring security In Memory users:
    -- Added part to create in memory users in spring security with no password encoder.

--- SpringSecurity/src/main/java/com/sherry/SpringSecurity/config/ProjectSecurityConfig.java
+++ SpringSecurity/src/main/java/com/sherry/SpringSecurity/config/ProjectSecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -18,9 +22,16 @@ public class ProjectSecurityConfig {
         http.authorizeHttpRequests((requests) ->
                         requests.requestMatchers("/myBalance", "/myAccount", "/myCards", "/loans").authenticated()
                                 .requestMatchers("/welcome", "/notices", "/error").permitAll());
-        http.formLogin(fle -> fle.disable());
+        http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user").password("{noop}12345").authorities("read").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}12345").authorities("admin").build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
 }
