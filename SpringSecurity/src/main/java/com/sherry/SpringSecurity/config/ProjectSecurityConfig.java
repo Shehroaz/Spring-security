c8a9f71bb33ed3bccafbdc4da84149ad9533588a
Shehroaz Aslam <muhammadshehroaz@gmail.com>
2026-04-29 13:07:23 +0500

    Adding functionality to authenticate user from mysql db using JdbcUserDetailsManager

--- SpringSecurity/src/main/java/com/sherry/SpringSecurity/config/ProjectSecurityConfig.java
+++ SpringSecurity/src/main/java/com/sherry/SpringSecurity/config/ProjectSecurityConfig.java
@@ -5,13 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -30,10 +30,8 @@ public class ProjectSecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user").password("{bcrypt}$2a$12$KY/h80uXsPKFxJXoogXEEufFDYXWkT2CRua88OFIhoR3H/H/g3qsC").authorities("read").build();
-        UserDetails admin = User.withUsername("admin").password("{noop}12345").authorities("admin").build();
-        return new InMemoryUserDetailsManager(user, admin);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+       return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
